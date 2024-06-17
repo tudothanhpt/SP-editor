991692d4dfb6bade53dff7a9fbe53dbf7e57058b
ABui <buiquanganh1298@gmail.com>
2024-06-17 15:49:39 +0700

    Adding functions

---
+++ src/sp_editor/crud/cr_sectiondesigner.py
@@ -0,0 +1,101 @@
+import sys
+import pandas as pd
+from pandas import DataFrame
+
+from sp_editor.database.models import LoadCombinations, LoadCombinationsSelection
+from sqlmodel import Session, create_engine
+from sqlalchemy.engine.base import Engine
+from sp_editor.core.connect_etabs import connect_to_etabs
+
+TB_UNIQUECOMBO = str(LoadCombinations.__name__).lower()
+TB_COMBOSELECTION = str(LoadCombinationsSelection.__name__).lower()
+TB_CS_HEADER_ORICOMBO= list(LoadCombinationsSelection.__fields__.keys())[1]
+TB_CS_HEADER_SELECTEDCOMBO=list(LoadCombinationsSelection.__fields__.keys())[2]
+
+def create_loadComboDB(engine, df: pd.DataFrame):
+    """
+    Writes a DataFrame to the 'loadcombinations' table in the database.
+
+    Args:
+        engine (sqlalchemy.engine.Engine): The database engine.
+        df (pd.DataFrame): The DataFrame containing data to write.
+
+    Raises:
+        Exception: If an error occurs during the write operation.
+    """
+    try:
+        # Write DataFrame to SQL
+        # Write DataFrame to SQL
+        df.to_sql(TB_UNIQUECOMBO,  # Table name
+                  con=engine,  # SQLAlchemy engine
+                  if_exists='replace',  # 'replace' if table already exists
+                  index=False)  # Do not write index as a column
+        print(f"DataFrame successfully written to table '{TB_UNIQUECOMBO}' in the database.")
+
+    except Exception as e:
+        # If an error occurs, print the error message
+        print(f"An error occurred: {e}")
+        
+def create_loadComboSelectionDB(engine, df: pd.DataFrame):
+    """
+    Writes a DataFrame to the 'loadcombinationsselection' table in the database.
+
+    Args:
+        engine (sqlalchemy.engine.Engine): The database engine.
+        df (pd.DataFrame): The DataFrame containing data to write.
+
+    Raises:
+        Exception: If an error occurs during the write operation.
+    """
+    try:
+        # Write DataFrame to SQL
+        # Write DataFrame to SQL
+        df.to_sql(
+            TB_COMBOSELECTION,  # Table name
+            con=engine,  # SQLAlchemy engine
+            if_exists='replace',  # 'replace' if table already exists
+            index=False  # Do not write index as a column
+        )
+        print(
+            f"DataFrame successfully written to table '{TB_COMBOSELECTION}' in the database."
+        )
+    except Exception as e:
+        # If an error occurs, print the error message
+        print(f"An error occurred: {e}")
+
+def read_loadComboDB(engine):
+    """
+    Reads a SQL table into a pandas DataFrame.
+
+    Parameters:
+    engine (sqlalchemy.engine.Engine): The SQLAlchemy engine connected to the database.
+
+    Returns:
+    pd.DataFrame: The DataFrame containing the table data.
+    """
+    # Read SQL table into a DataFrame
+    df = pd.read_sql_table(
+        table_name=TB_UNIQUECOMBO,  # Table name
+        con=engine                  # SQLAlchemy engine
+    )
+
+    return df
+
+def read_loadComboSelectionDB(engine):
+    """
+    Reads the SQL table `TB_COMBOSELECTION` into a pandas DataFrame.
+
+    Parameters:
+    engine (sqlalchemy.engine.Engine): The SQLAlchemy engine connected to the database.
+
+    Returns:
+    pd.DataFrame: The DataFrame containing the table data.
+    """
+    # Read SQL table into a DataFrame
+    df = pd.read_sql_table(
+        table_name=TB_COMBOSELECTION,  # The table to read
+        con=engine                     # The SQLAlchemy engine
+    )
+
+    return df # The DataFrame containing the table data
+
---
+++ tests/TestBM/find_uniform_bars.py
@@ -0,0 +1,171 @@
+import math
+from typing import List, Tuple, Dict
+from connect_etabs import connect_to_etabs, get_sdshape_pierPolygon, restructure_sdshapeDF, spColumn_CTI_PierPoint
+from shapely.geometry import Polygon
+from shapely.geometry.polygon import LinearRing
+import matplotlib.pyplot as plt
+
+# Define type aliases for better readability and maintainability
+X = float
+Y = float
+POINT = Tuple[X, Y]
+POLYLINE = List[POINT]
+PIERSDSHAPE = Dict[str, List[POLYLINE]]
+LST_PIERSDSHAPE = List[PIERSDSHAPE]
+
+def plot_polygons(polygons,shapes,rebar_list):
+    """
+    Plots a list of Shapely polygons using Matplotlib.
+
+    :param polygons: List of Shapely Polygon objects to be plotted.
+    :param fill: Boolean indicating whether to fill the polygons.
+    :param alpha: Alpha value for polygon fill transparency.
+    :param edge_color: Color of the polygon edges.
+    :param edge_width: Width of the polygon edges.
+    """
+    
+    plt.figure(figsize=(8, 8))
+    
+    for polygon in polygons:
+        x, y = zip(*polygon)
+        #plt.plot(x, y, color='b', linewidth=1, linestyle='--')
+
+    for shape in shapes:
+        x, y = zip(*shape)
+        plt.plot(x, y, color='r', linewidth=1.5)
+    
+    x, y = zip(*rebar_list)
+    plt.scatter(x, y, color='g',s=1)
+    
+    plt.title('Polygons')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+
+    plt.show()
+
+def offset_sdshapeDF(lst_PierSDShape, tier_name='Tier1.0', offset_distance=-5):
+    """
+    Processes the input DataFrame to extract shapes, apply an offset, and return the modified coordinates.
+
+    :param tier_name: The name of the tier to filter shapes.
+    :param offset_distance: The distance to offset the shapes.
+    :param join_style: The join style for the offset.
+    :return: List of coordinates for the offset shapes.
+    """
+
+    lst_offsetted_shapes = []
+    area = 0
+    # Iterate over the extracted shapes
+    for tier_dict in lst_PierSDShape:
+        if tier_name in tier_dict:
+            shapes = tier_dict[tier_name]
+            for shape in shapes:
+                # Create a polygon and apply the offset
+                polygon = Polygon(shape)
+                area += (polygon.area)
+
+                offset_polygon = polygon.buffer(offset_distance, join_style="mitre")
+                offset_polygon_coords = [(round(x, 2), round(y, 2)) for x, y in offset_polygon.exterior.coords]
+                lst_offsetted_shapes.append(offset_polygon_coords)
+
+    print(area)
+    
+    return lst_offsetted_shapes
+
+def extract_segments(lst_offsetted_shapes):
+
+    """
+    Input polyline to be extracted as list
+    Return Values:
+    list_segments (list) : List of  segments forming the polyline
+
+    """
+    segments_shapes = []
+    for shape in lst_offsetted_shapes:
+        num_points = len(shape)
+        for i in range(num_points - 1):
+            segment = [shape[i], shape[i + 1]]
+            segments_shapes.append(segment)          
+    return segments_shapes
+
+def calculate_rebarpoints_for_segments(lst_offsetted_shapes, spacing):
+    """
+    @lst_polyline: list of offsetted polyline (with cover and half rebar diameter)
+    @spacing: rebar spacing (user defined)
+    Return Values:
+    list_rebarsPts (list) : List of rebars' coordinates based on polyline segments and user-specified spacing
+    """
+    # Break polyline into segments 
+    segments_shapes = extract_segments(lst_offsetted_shapes)
+    
+    temp_list_rebarsPts = []
+    for segment in segments_shapes:
+        start_point, end_point = segment
+        # Calculate the control points for the current segment
+        rounded_distance = math.sqrt(
+            (end_point[0] - start_point[0]) ** 2 + (end_point[1] - start_point[1]) ** 2)
+        num_points = math.ceil(rounded_distance / spacing)
+        num_points = max(num_points, 2)
+        x_diff = (end_point[0] - start_point[0]) / (num_points - 1)
+        y_diff = (end_point[1] - start_point[1]) / (num_points - 1)
+        control_points = [(round(start_point[0] + i * x_diff, 2), round(start_point[1] + i * y_diff, 2)) for i
+                            in range(num_points)]
+        # Append the calculated rebars point to the list of unique values
+        temp_list_rebarsPts.append(control_points)
+        
+        
+        # Remove duplicates from the list of rebars point
+    list_rebarsPts = []
+
+    for sublist in temp_list_rebarsPts:
+        for lst_coordinates in sublist:
+            list_rebarsPts.append(lst_coordinates)
+    list_rebarsPts =list(set(list_rebarsPts))        
+    return list_rebarsPts
+
+def spColumn_CTI_Rebar(list_rebarsPts, rebarArea):
+    """
+    Converts a list of tuples into a multiline string format with a parameter added to each tuple.
+
+    Args:
+    - lst_rebarCoordiantes (list): The original list of tuples or lists of rebar coordiantes.
+    - parameter: rebarArea.
+
+    Returns:
+    - str: The multiline string.
+    """
+    if isinstance(list_rebarsPts[0], tuple):
+        modified_list = [(rebarArea,) + item for item in list_rebarsPts]
+        modified_list = [(f"{area:.6f}", f"{x:.6f}", f"{y:.6f}") for (area, x, y) in modified_list]
+    elif isinstance(list_rebarsPts[0], list):
+        modified_list= [[rebarArea] + item for item in list_rebarsPts]
+        modified_list = [(f"{area:.6f}", f"{x:.6f}", f"{y:.6f}") for [area, x, y] in modified_list]
+    else:
+        raise TypeError("Unsupported data format. Must be list of tuples or lists.")
+    
+    multiline_string_rebarPts = '\n'.join([','.join(map(str, item)) for item in modified_list])
+    total_rebar = len(modified_list)
+    multiline_string_rebarPts = str(total_rebar) + '\n' + multiline_string_rebarPts
+    
+    return multiline_string_rebarPts
+
+def main():
+    sap_model = connect_to_etabs(model_is_open=True)[0]
+    df_sd= get_sdshape_pierPolygon(sap_model)
+    lst_PierSDShape=restructure_sdshapeDF(df_sd)
+    tier_name = 'Tier1.0'
+    
+    offsetted_shapes = offset_sdshapeDF(lst_PierSDShape, tier_name=tier_name, offset_distance=-5)
+    formatted_output = spColumn_CTI_PierPoint(lst_PierSDShape, tier_name)
+    segments_shapes=extract_segments(offsetted_shapes)
+    
+    rebar_list = calculate_rebarpoints_for_segments(offsetted_shapes, 24)
+    multiline_string_rebarPts = spColumn_CTI_Rebar(rebar_list, 0.5)
+    with open('sample.txt', 'w') as file:
+        file.write(multiline_string_rebarPts)
+    #print(formatted_output)
+    #print(multiline_string_rebarPts)
+    plot_polygons(offsetted_shapes,lst_PierSDShape[0][tier_name],rebar_list)
+    
+if __name__ == "__main__":
+    main() 
