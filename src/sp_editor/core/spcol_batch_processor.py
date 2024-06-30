4722b247a88c66b8191329de00e54b2d1f0da84e
Anhbq1298 <145368314+Anhbq1298@users.noreply.github.com>
2024-06-30 17:01:19 +0700

    commit

--- src/sp_editor/core/spcol_batch_processor.py
+++ src/sp_editor/core/spcol_batch_processor.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import subprocess
-import pandas as pd
-import os
-import sys
-#from sp_editor.core.cti_data_merging import read_summaryCTI_DB
-from sqlmodel import Session,create_engine
-from sqlalchemy.engine.base import Engine
-from sp_editor.utils import  get_engine_path
-
-from PySide6 import QtWidgets as qtw
-
-# Define the constant path to spColumn.CLI.exe
-SPCOLUMN_PATH = r"D:\spColumn\StructurePoint\spColumn\spColumn.CLI.exe"
-=======
 import os
 import subprocess
 import pandas as pd
@@ -21,7 +6,6 @@ from sqlalchemy.engine.base import Engine
 
 # DEFAULT BY INSTALLATION, PLEASE CHANGE THIS IF NOT COMPATIBLE WITH YOUR COMPUTER'S SETTINGS
 SPCOLUMN_PATH = r"C:\Program Files (x86)\StructurePoint\spColumn\spColumn.CLI.exe"
->>>>>>> 8c64ccd2a73bd296cb200aeea0859021b9ebeed6
 
 
 class BatchProcessorThread(QThread):
@@ -29,62 +13,6 @@ class BatchProcessorThread(QThread):
     success = Signal(str)
     output = Signal(str)
 
-<<<<<<< HEAD
-batch_file_path = r"C:\Users\abui\Desktop\Git\Repo\SP-editor\run_spColumn.bat"
-def create_batch_file(engine, batch_file_path):
-    options = ["/rcsv"]
-    try:
-        df_CTIsummary = read_summaryCTI_DB(engine)
-        input_files = df_CTIsummary["PathAfterCreation"].tolist()
-        filename = df_CTIsummary["ID2"].tolist()
-
-        with open(batch_file_path, 'w') as batch_file:
-            for i in range(len(df_CTIsummary)):
-                input_dir = os.path.dirname(input_files[i])
-                output_folder = os.path.join(input_dir, "outputs")
-                os.makedirs(output_folder, exist_ok=True)
-                output_file = os.path.join(output_folder, filename[i])
-
-                batch_file.write(f'"{SPCOLUMN_PATH}" /i:"{input_files[i]}" /o:"{output_file}.out" ' + ' '.join(options) + '\n')
-
-        return True, f"Batch file created successfully: {batch_file_path}"
-
-    except FileNotFoundError:
-        error_message = f"The file '{SPCOLUMN_PATH}' was not found. Please check the path and try again."
-        return False, error_message
-
-    except Exception as e:
-        error_message = f"An unexpected error occurred: {e}"
-        return False, error_message
-
-def show_message_box(title, message):
-
-    msg = qtw.QMessageBox()
-    msg.setIcon(qtw.QMessageBox.Critical)
-    msg.setWindowTitle(title)
-    msg.setText(message)
-    msg.exec_()
-
-def read_summaryCTI_DB(engine):
-    """
-    """
-    # Read SQL table into a DataFrame
-    df = pd.read_sql_table(
-        table_name="ctisummary",  # The table to read
-        con=engine  # The SQLAlchemy engine
-    )
-    return df  # The DataFrame containing the table data
-
-if __name__ == "__main__":
-    # Example SQLAlchemy engine setup (SQLite in this case)
-    engine_temppath = r"C:\Users\AnhBui\Desktop\testCTI\12345.spe"
-    engine = create_engine(f"sqlite:///{engine_temppath}")
-    engine_path = get_engine_path(engine)
-    batch_file_path = os.path.join(engine_path, "run_spColumn.bat")
-    create_batch_file(engine, batch_file_path)
-    print("done")
-    os.startfile(batch_file_path)
-=======
     def __init__(self, engine: Engine, input_files: list[str], options: list[str] = ["/rcsv"]):
         super().__init__()
         self.engine = engine
@@ -168,4 +96,3 @@ if __name__ == "__main__":
         except Exception as e:
             error_message = f"An unexpected error occurred: {e}"
             self.error.emit(error_message)
->>>>>>> 8c64ccd2a73bd296cb200aeea0859021b9ebeed6
--- src/sp_editor/widgets/batch_processor_dialog_ui.py
+++ src/sp_editor/widgets/batch_processor_dialog_ui.py
@@ -1,56 +1,159 @@
-from PySide6 import QtCore as qtc
-from PySide6 import QtWidgets as qtw
-from PySide6 import QtGui as qtg
+# -*- coding: utf-8 -*-
 
+################################################################################
+## Form generated from reading UI file 'batch_processor_dialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QDialog, QFrame,
+    QGridLayout, QGroupBox, QHBoxLayout, QLabel,
+    QPushButton, QSizePolicy, QSpacerItem, QTextEdit,
+    QVBoxLayout, QWidget)
 
 class Ui_BatchProcessorDialog(object):
     def setupUi(self, BatchProcessorDialog):
-        BatchProcessorDialog.setObjectName("BatchProcessorDialog")
-        BatchProcessorDialog.resize(500, 400)
-        BatchProcessorDialog.setMinimumSize(qtc.QSize(500, 400))
-        BatchProcessorDialog.setMaximumSize(qtc.QSize(500, 400))
+        if not BatchProcessorDialog.objectName():
+            BatchProcessorDialog.setObjectName(u"BatchProcessorDialog")
+        BatchProcessorDialog.resize(500, 600)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(BatchProcessorDialog.sizePolicy().hasHeightForWidth())
+        BatchProcessorDialog.setSizePolicy(sizePolicy)
+        BatchProcessorDialog.setMinimumSize(QSize(500, 600))
+        BatchProcessorDialog.setMaximumSize(QSize(500, 600))
+        self.verticalLayout = QVBoxLayout(BatchProcessorDialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label_tracking_screen = QLabel(BatchProcessorDialog)
+        self.label_tracking_screen.setObjectName(u"label_tracking_screen")
+        font = QFont()
+        font.setFamilies([u"Segoe UI"])
+        font.setPointSize(12)
+        font.setBold(True)
+        self.label_tracking_screen.setFont(font)
+        self.label_tracking_screen.setFrameShape(QFrame.Shape.Box)
+        self.label_tracking_screen.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.verticalLayout = qtw.QVBoxLayout(BatchProcessorDialog)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.addWidget(self.label_tracking_screen)
 
-        self.frame = qtw.QFrame(BatchProcessorDialog)
-        self.frame.setObjectName("frame")
-        self.frame.setFrameShape(qtw.QFrame.Shape.Box)
-        self.frame.setFrameShadow(qtw.QFrame.Shadow.Plain)
+        self.frame = QFrame(BatchProcessorDialog)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFocusPolicy(Qt.FocusPolicy.TabFocus)
+        self.frame.setFrameShape(QFrame.Shape.Box)
+        self.frame.setFrameShadow(QFrame.Shadow.Plain)
         self.frame.setLineWidth(1)
-        self.frame_layout = qtw.QVBoxLayout(self.frame)
-
-        self.t_resultTextEdit = qtw.QTextEdit(self.frame)
-        self.t_resultTextEdit.setObjectName("t_resultTextEdit")
+        self.frame.setMidLineWidth(1)
+        self.frame_layout = QVBoxLayout(self.frame)
+        self.frame_layout.setObjectName(u"frame_layout")
+        self.t_resultTextEdit = QTextEdit(self.frame)
+        self.t_resultTextEdit.setObjectName(u"t_resultTextEdit")
+        self.t_resultTextEdit.setMinimumSize(QSize(0, 0))
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI"])
+        font1.setPointSize(11)
+        self.t_resultTextEdit.setFont(font1)
+        self.t_resultTextEdit.setFrameShape(QFrame.Shape.Panel)
+        self.t_resultTextEdit.setFrameShadow(QFrame.Shadow.Sunken)
+        self.t_resultTextEdit.setLineWidth(0)
+        self.t_resultTextEdit.setTabChangesFocus(False)
         self.t_resultTextEdit.setReadOnly(True)
-        self.t_resultTextEdit.setFont(qtg.QFont("Segoe UI", 11))
+
         self.frame_layout.addWidget(self.t_resultTextEdit)
+
 
         self.verticalLayout.addWidget(self.frame)
 
-        self.verticalSpacer = qtw.QSpacerItem(20, 20, qtw.QSizePolicy.Policy.Minimum, qtw.QSizePolicy.Policy.Fixed)
+        self.verticalSpacer = QSpacerItem(0, 10, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+
         self.verticalLayout.addItem(self.verticalSpacer)
 
-        self.horizontalLayout = qtw.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.typeGroupBox = QGroupBox(BatchProcessorDialog)
+        self.typeGroupBox.setObjectName(u"typeGroupBox")
+        font2 = QFont()
+        font2.setFamilies([u"Segoe UI"])
+        font2.setPointSize(12)
+        self.typeGroupBox.setFont(font2)
+        self.typeLayout = QGridLayout(self.typeGroupBox)
+        self.typeLayout.setObjectName(u"typeLayout")
+        self.cb_excel = QCheckBox(self.typeGroupBox)
+        self.cb_excel.setObjectName(u"cb_excel")
+        self.cb_excel.setEnabled(False)
+        self.cb_excel.setChecked(True)
 
-        self.pb_startButton = qtw.QPushButton(BatchProcessorDialog)
-        self.pb_startButton.setObjectName("pb_startButton")
-        self.pb_startButton.setFont(qtg.QFont("Segoe UI", 12))
+        self.typeLayout.addWidget(self.cb_excel, 0, 0, 1, 1)
+
+        self.cb_word = QCheckBox(self.typeGroupBox)
+        self.cb_word.setObjectName(u"cb_word")
+
+        self.typeLayout.addWidget(self.cb_word, 0, 1, 1, 1)
+
+        self.cb_pdf = QCheckBox(self.typeGroupBox)
+        self.cb_pdf.setObjectName(u"cb_pdf")
+
+        self.typeLayout.addWidget(self.cb_pdf, 0, 2, 1, 1)
+
+        self.cb_text = QCheckBox(self.typeGroupBox)
+        self.cb_text.setObjectName(u"cb_text")
+
+        self.typeLayout.addWidget(self.cb_text, 1, 0, 1, 1)
+
+        self.cb_csv = QCheckBox(self.typeGroupBox)
+        self.cb_csv.setObjectName(u"cb_csv")
+
+        self.typeLayout.addWidget(self.cb_csv, 1, 1, 1, 1)
+
+        self.cb_dxf = QCheckBox(self.typeGroupBox)
+        self.cb_dxf.setObjectName(u"cb_dxf")
+
+        self.typeLayout.addWidget(self.cb_dxf, 1, 2, 1, 1)
+
+
+        self.verticalLayout.addWidget(self.typeGroupBox)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pb_startButton = QPushButton(BatchProcessorDialog)
+        self.pb_startButton.setObjectName(u"pb_startButton")
+        self.pb_startButton.setFont(font2)
+
         self.horizontalLayout.addWidget(self.pb_startButton)
 
-        self.pb_showButton = qtw.QPushButton(BatchProcessorDialog)
-        self.pb_showButton.setObjectName("pb_showButton")
-        self.pb_showButton.setFont(qtg.QFont("Segoe UI", 12))
+        self.pb_showButton = QPushButton(BatchProcessorDialog)
+        self.pb_showButton.setObjectName(u"pb_showButton")
+        self.pb_showButton.setFont(font2)
+
         self.horizontalLayout.addWidget(self.pb_showButton)
+
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
+
         self.retranslateUi(BatchProcessorDialog)
-        qtc.QMetaObject.connectSlotsByName(BatchProcessorDialog)
+
+        QMetaObject.connectSlotsByName(BatchProcessorDialog)
+    # setupUi
 
     def retranslateUi(self, BatchProcessorDialog):
-        _translate = qtc.QCoreApplication.translate
-        BatchProcessorDialog.setWindowTitle(_translate("BatchProcessorDialog", "Batch Processor"))
-        self.pb_startButton.setText(_translate("BatchProcessorDialog", "Batch Proccesing"))
-        self.pb_showButton.setText(_translate("BatchProcessorDialog", "Show Result"))
+        BatchProcessorDialog.setWindowTitle(QCoreApplication.translate("BatchProcessorDialog", u"Batch Processor", None))
+        self.label_tracking_screen.setText(QCoreApplication.translate("BatchProcessorDialog", u"TRACKING SCREEN", None))
+        self.typeGroupBox.setTitle(QCoreApplication.translate("BatchProcessorDialog", u"Output Type", None))
+        self.cb_excel.setText(QCoreApplication.translate("BatchProcessorDialog", u"Excel", None))
+        self.cb_word.setText(QCoreApplication.translate("BatchProcessorDialog", u"Word", None))
+        self.cb_pdf.setText(QCoreApplication.translate("BatchProcessorDialog", u"PDF", None))
+        self.cb_text.setText(QCoreApplication.translate("BatchProcessorDialog", u"Text", None))
+        self.cb_csv.setText(QCoreApplication.translate("BatchProcessorDialog", u"CSV", None))
+        self.cb_dxf.setText(QCoreApplication.translate("BatchProcessorDialog", u"DXF", None))
+        self.pb_startButton.setText(QCoreApplication.translate("BatchProcessorDialog", u"Batch Processing", None))
+        self.pb_showButton.setText(QCoreApplication.translate("BatchProcessorDialog", u"Show Result", None))
+    # retranslateUi
+
