01e6a32d21f1c0989279c4ebbfff448233796b7f
Saitoh Chido <dothanhtupt@gmail.com>
2024-11-29 09:59:24 +0700

    add new EC material table

--- src/sp_editor/controllers/about_dialog.py
+++ src/sp_editor/controllers/about_dialog.py
@@ -5,6 +5,7 @@ from PySide6 import QtGui as qtg
 
 from sp_editor.widgets.about_dialog_ui import Ui_d_About
 
+
 class AboutDialog(qtw.QDialog, Ui_d_About):
 
     def __init__(self, parent: qtw.QWidget = None):
@@ -12,13 +13,13 @@ class AboutDialog(qtw.QDialog, Ui_d_About):
         self.setupUi(self)
         self.pb_OK.clicked.connect(self.on_ok_clicked)
         self.label.setPixmap(qtg.QPixmap(u"icons/SP-EDITOR_PNG.png"))
-        
+
     def on_ok_clicked(self):
         self.close()
 
+
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
-
     window = AboutDialog()
     window.show()
     sys.exit(app.exec())
