e911417094618664642abe5d65ed46a3ac11a4bf
Tado <dothanhtupt@gmail.com>
2024-12-11 22:31:30 +0700

    update uv for package management

--- src/sp_editor/__init__.py
+++ src/sp_editor/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from sp-editor!"
+from sp_editor._version import __version__
+import os
+
+GLOBALPATH =os.path.abspath(os.path.dirname(__file__))
+print(GLOBALPATH)
